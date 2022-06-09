0b54e454169d83ba088b0a04ee0036af42ec7460
Eric Martindale <eric@ericmartindale.com>
2022-06-08 23:26:58 -0400

    Update general UI

--- components/FeedMonitor.js
+++ components/FeedMonitor.js
@@ -1,30 +1,36 @@
 import React from 'react';
 import '../styles/feed.css';
 // import '../libraries/fomantic/dist/semantic.css';
+
 import {
+  Card,
+  Header,
   Segment
 } from 'semantic-ui-react';
 
-// import FabricBridge from '@fabric/react';
+// Internal Components
 import Feed from './Feed';
+import Quote from './Quote';
+import Rate from './Rate';
 
 export default class FeedMonitor extends React.Component {
   state = {
-    currency: 'BTC',
-    prices: {
-      'BTC': 1
-    },
-    quote: {
-      created: (new Date()).toISOString(),
-      currency: 'BTC',
-      rate: 1
-    }
+    currency: 'USD',
+    symbols: ['BTC', 'NMC', 'LTC'],
+    quotes: [
+      {
+        value: 29349.54,
+        currency: 'USD',
+        symbol: 'BTC'
+      }
+    ]
   }
 
   constructor (props = {}) {
     super(props);
 
     this._state = {
+      assets: {},
       content: this.state // TODO: inherit get state () from Actor
     };
 
@@ -47,7 +53,36 @@ export default class FeedMonitor extends React.Component {
     return (
       <fabric-content-page class="ui page">
         <Segment>
+          <Header><h1>Price</h1></Header>
           <Feed />
+
+          <Header><h2>Symbols</h2></Header>
+          <div class="ui cards">
+            {this.state.symbols.map((symbol) => {
+              return (
+                <Card>
+                  <Card.Content>
+                    <Header>{symbol}</Header>
+                    <Rate currency={this.state.currency} symbol={symbol} />
+                  </Card.Content>
+                </Card>
+              );
+            })}
+          </div>
+
+          <Header><h2>Quotes</h2></Header>
+          <div class="ui cards">
+            {this.state.quotes.map((quote, i) => {
+              return (
+                <Card>
+                  <Card.Content>
+                    <Header><strong>Quote #{i + 1} (quotes[{i}])</strong></Header>
+                    <Quote symbol={quote.symbol} currency={quote.currency} price={quote.price} />
+                  </Card.Content>
+                </Card>
+              );
+            })}
+          </div>
         </Segment>
         {/* <FabricBridge host="localhost" secure="false" port="3000" /> */}
       </fabric-content-page>
